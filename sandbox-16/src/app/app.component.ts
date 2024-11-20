1161c0c0c7b054d14af5ae997627f64a6aa375a8
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-11-20 23:00:26 +0700

    rxjs

--- sandbox-16/src/app/app.component.ts
+++ sandbox-16/src/app/app.component.ts
@@ -1,17 +1,35 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
 import {NameService} from './name.service';
 import {ComService} from './com.service';
+import {
+  concat,
+  concatMap,
+  exhaustMap,
+  fromEvent,
+  interval,
+  mergeMap,
+  Observable,
+  switchMap,
+  takeUntil,
+  takeWhile
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements AfterViewChecked{
   title = 'sandbox-16';
 
   public ns = inject(NameService)
   public cs = inject(ComService)
+
+  evenObs$!: Observable<any>;
+
+  @ViewChild('clickBox')
+  btn!: ElementRef
+
 
   changeName() {
     this.ns.name = "Pierre"
@@ -20,4 +38,17 @@ export class AppComponent{
   sendName() {
     this.cs.mes$.next(this.ns.name)
   }
+
+  ngAfterViewChecked(): void {
+    if(!this.evenObs$){
+      this.evenObs$ =  fromEvent(this.btn.nativeElement, 'click').pipe(
+        exhaustMap(v => {
+          return interval(500).pipe(takeWhile(w => w <= 5))
+        }
+        )
+      )
+      this.evenObs$.subscribe(v => console.log(v))
+    }
+  }
+
 }
