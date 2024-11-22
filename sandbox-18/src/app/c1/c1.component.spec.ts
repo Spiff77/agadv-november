70fe325cd92ab282289a491fb8ffab3e69bdecaa
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-11-22 23:17:53 +0700

    support

--- sandbox-18/src/app/c1/c1.component.spec.ts
+++ sandbox-18/src/app/c1/c1.component.spec.ts
@@ -1,18 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { C1Component } from './c1.component';
+import { CustomFormContainerComponent } from './custom-form-container.component';
+import {ControlContainer} from '@angular/forms';
 
-describe('C1Component', () => {
-  let component: C1Component;
-  let fixture: ComponentFixture<C1Component>;
+describe('CustomFormContainerComponent', () => {
+  let component: CustomFormContainerComponent;
+  let fixture: ComponentFixture<CustomFormContainerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [C1Component]
+      imports: [CustomFormContainerComponent],
+      providers:[ControlContainer]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(C1Component);
+    fixture = TestBed.createComponent(CustomFormContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
